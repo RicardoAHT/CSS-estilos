11c19eaa4e347adcc420aa65b71623696a26ea4e
Ricardo <rickxd007@gmail.com>
2023-12-16 22:23:27 -0500

    Realizo boton con animacion en el hover para simular un borde que cambia de color

--- src/components/ContainerColumn/ContainerColumn.tsx
+++ src/components/ContainerColumn/ContainerColumn.tsx
@@ -3,7 +3,14 @@
 const ContainerColumn = () => {
   return (
     <div className='containerColumn'>
+      <h1>
       Container Column
+      </h1>
+      <button className="buttonBorderColor">
+        <div className="buttonBorderColor__div">
+          Go!
+        </div>
+      </button>
     </div>
   )
 }
