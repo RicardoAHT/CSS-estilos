b62b4a7d95beda134c34cd78c3134962ca99e435
Ricardo <rickxd007@gmail.com>
2023-12-23 17:33:35 -0500

    Instalo reactHookForm en el proyecto

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,16 @@ import ImageAppear from './components/ImagesAppear/ImageAppear'
 import ListRotateGrid from "./components/ListRotateGrid/ListRotateGrid"
 import Loading from "./components/Loading/Loading"
 import CounterElements from './components/CounterElements/CounterElements'
+import BasicHookForm from './components/ReactHookForm/BasicHookForm'
 
 const App = () => {
   return (
     <div className='app'>
       <div className='containerColumn'>
         <ButtonBorderColor/>
+      </div>
+      <div className='containerColumn'>
+        <BasicHookForm/>
       </div>
       <div className='containerColumn'>
         <h2>.counterElement</h2>
