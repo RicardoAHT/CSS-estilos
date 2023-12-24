b26a7f89d9dbe5651d02425a1d2115f18e9bf6ef
Ricardo <rickxd007@gmail.com>
2023-12-23 22:17:18 -0500

    Practico el uso de ReactHookForm

--- src/components/ReactHookForm/BasicHookForm.tsx
+++ src/components/ReactHookForm/BasicHookForm.tsx
@@ -9,18 +9,25 @@ export default function BasicHookForm() {
   const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = data => console.log(data);
 
-  console.log(watch("example")) // watch input value by passing the name of it
-
+  //console.log(watch("example")) con watch puedes ver el input value pasando el nombre del input
+    // si no le pasas parametros a watch te muestra todos los inputs pord defecto
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
     <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
-      <input defaultValue="test" {...register("example")} />
+      <div>
+        <label >1er input </label>
+        <input defaultValue="test" {...register("example", {required: true})} />
+        {errors.example && <span>This field is required 1</span>}
+      </div>
       
       {/* include validation with required or other standard HTML validation rules */}
-      <input {...register("exampleRequired", { required: true })} />
+      <div>
+        <label >2do Input </label>
+        <input {...register("exampleRequired", { required: true, minLength: {value:4, message:"min length es 4"} })} />
+      </div>
       {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+      {errors.exampleRequired && <span>This field is required 2</span>}
       
       <input type="submit" />
     </form>
